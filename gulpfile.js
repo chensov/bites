cac12e1a0ea340061c34d7dce76b350e6bda32b2
chensov <senche.dev@gmail.com>
2023-10-04 12:13:44 +0100

    add browerSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
-const { src, dest, watch, series } = require("gulp");
+const { src, dest, watch, series, task } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
 
+// Compile SASS into CSS
 function buildStyles() {
-  return src("resources/sass/**/*.scss") // Updated the source path
-    .pipe(sass().on("error", sass.logError)) // To log errors instead of failing silently
-    .pipe(dest("web/css")); // Updated the destination path
+  return src("resources/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("web/css"))
+    .pipe(browserSync.stream()); // Stream changes to BrowserSync
 }
 
+// Watch changes in files
 function watchTask() {
-  watch(["resources/sass/**/*.scss"], buildStyles); // Updated the watch path
+  watch("resources/sass/**/*.scss", buildStyles);
+  watch("templates/**/*.twig").on("change", browserSync.reload); // Updated path for twig templates
+  watch("web/css/*.css").on("change", browserSync.reload); // Updated path for css files
+  // If you have JS files or other assets you can add more watch statements similarly
 }
 
-exports.default = series(buildStyles, watchTask);
+// BrowserSync Task
+function serve() {
+  browserSync.init({
+    proxy: "http://craftbites.ddev.site/",
+    browser: "google chrome", // This tells BrowserSync to open the site with Google Chrome
+  });
+  watchTask(); // Incorporate watchTask into the serve task
+}
+
+// Define tasks using the new Gulp API format
+exports.buildStyles = buildStyles;
+exports.watch = watchTask;
+exports.serve = serve;
+exports.default = serve; // Making the serve task the default
