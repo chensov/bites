ce3a47bedced5dfe682ff65922168dc3fc6ee25f
chensov <senche.dev@gmail.com>
2023-10-04 09:53:45 +0100

    sass/css directories

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("resources/sass/**/*.scss") // Updated the source path
+    .pipe(sass().on("error", sass.logError)) // To log errors instead of failing silently
+    .pipe(dest("web/css")); // Updated the destination path
 }
 
 function watchTask() {
-  watch(["sass/**/*.scss"], buildStyles);
+  watch(["resources/sass/**/*.scss"], buildStyles); // Updated the watch path
 }
 
 exports.default = series(buildStyles, watchTask);
