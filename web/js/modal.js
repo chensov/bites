f3677abe70420b450f0a3db30a40fb9b9c048161
chensov <senche.dev@gmail.com>
2023-10-05 19:13:29 +0100

    complete regestration form

--- web/js/modal.js
+++ web/js/modal.js
@@ -1,6 +1,9 @@
 const loginBtn = document.querySelector(".login-btn");
 const modal = document.querySelector(".login-modal");
 const closeModal = document.querySelector(".close-modal");
+const signUp = document.querySelector(".sign--up-btn");
+const registerModal = document.querySelector(".register-modal");
+const closeModalReg = document.querySelector(".close-modal-reg");
 
 loginBtn.addEventListener("click", () => {
   modal.classList.remove("hidden");
@@ -8,4 +11,12 @@ loginBtn.addEventListener("click", () => {
 
 closeModal.addEventListener("click", () => {
   modal.classList.add("hidden");
+});
+
+signUp.addEventListener("click", () => {
+  registerModal.classList.remove("hidden");
+});
+
+closeModalReg.addEventListener("click", () => {
+  registerModal.classList.add("hidden");
 });
