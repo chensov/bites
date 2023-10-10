b1d1034b1640860a82990504fe66cf1e29ac2cc2
chensov <senche.dev@gmail.com>
2023-10-10 15:25:51 +0100

    add recipe page

--- web/js/like.js
+++ web/js/like.js
@@ -1,7 +1,9 @@
 const likeBtns = document.querySelectorAll(".fa-heart");
 
 likeBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
     btn.classList.toggle("color-toggle");
   });
 });
