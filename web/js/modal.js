400cb1e05dd75ce31a24486821a5db2496544150
chensov <senche.dev@gmail.com>
2023-10-17 15:19:10 +0100

    update

--- web/js/modal.js
+++ web/js/modal.js
@@ -1,32 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const loginBtn = document.querySelector(".login-btn");
-  const modal = document.querySelector(".login-modal");
-  const closeModal = document.querySelector(".close-modal");
-  const signUp = document.querySelector(".sign--up-btn");
-  const registerModal = document.querySelector(".register-modal");
-  const closeModalReg = document.querySelector(".close-modal-reg");
+const modal = document.querySelector(".login-modal");
+const registerModal = document.querySelector(".register-modal");
 
-  if (loginBtn && modal) {
-    loginBtn.addEventListener("click", () => {
-      modal.classList.remove("hidden");
-    });
+// Event Delegation for Dynamically Loaded Content
+document.body.addEventListener("click", function (event) {
+  // For the login button
+  if (event.target.matches(".login-btn")) {
+    modal.classList.remove("hidden");
   }
 
-  if (closeModal && modal) {
-    closeModal.addEventListener("click", () => {
-      modal.classList.add("hidden");
-    });
+  // For the close modal button
+  if (event.target.matches(".close-modal")) {
+    modal.classList.add("hidden");
   }
 
-  if (signUp && registerModal) {
-    signUp.addEventListener("click", () => {
-      registerModal.classList.remove("hidden");
-    });
+  // For the sign-up button
+  if (event.target.matches(".sign--up-btn")) {
+    registerModal.classList.remove("hidden");
   }
 
-  if (closeModalReg && registerModal) {
-    closeModalReg.addEventListener("click", () => {
-      registerModal.classList.add("hidden");
-    });
+  // For the close register modal button
+  if (event.target.matches(".close-modal-reg")) {
+    registerModal.classList.add("hidden");
   }
 });
+
+const loginBtn = document.querySelector(".login-btn");
+const closeModal = document.querySelector(".close-modal");
+const signUp = document.querySelector(".sign--up-btn");
+const closeModalReg = document.querySelector(".close-modal-reg");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
+}
+
+if (closeModal) {
+  closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
+}
+
+if (signUp) {
+  signUp.addEventListener("click", () => {
+    registerModal.classList.remove("hidden");
+  });
+}
+
+if (closeModalReg) {
+  closeModalReg.addEventListener("click", () => {
+    registerModal.classList.add("hidden");
+  });
+}
